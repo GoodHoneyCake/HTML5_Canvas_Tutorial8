43e3fdbe29bfcf4fc069bd4d6e4d164496631256
kim <beomsu9696@daum.net>
2021-01-02 15:43:10 +0900

    add point.js

---
+++ .history/point_20210102153253.js
@@ -0,0 +1,11 @@
+export class Point {
+  constructor(x, y) {
+    this.x = x || 0;
+    this.y = y || 0;
+  }
+  add(point) {
+    this.x += point.x;
+    this.y += point.y;
+    return this;
+  }
+}
